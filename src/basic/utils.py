0da8897e4d5c50bab2356ec3307520c615af0cec
ForeverHaibara <69423537+ForeverHaibara@users.noreply.github.com>
2023-09-27 12:30:40 +0800

    radsimp

---
+++ src/basic/utils.py
@@ -0,0 +1,19 @@
+from typing import Union, List
+
+import sympy as sp
+
+def radsimp(expr: Union[sp.Expr, List[sp.Expr]]) -> sp.Expr:
+    """
+    Rationalize the denominator by removing square roots. Wrapper of sympy.radsimp.
+    Also refer to sympy.simplify.
+    """
+    if isinstance(expr, (list, tuple)):
+        return [radsimp(e) for e in expr]
+    if not isinstance(expr, sp.Expr):
+        expr = sp.sympify(expr)
+
+    numer, denom = expr.as_numer_denom()
+    n, d = sp.fraction(sp.radsimp(1/denom, symbolic=False, max_terms=1))
+    # if n is not S.One:
+    expr = (numer*n).expand()/d
+    return expr
